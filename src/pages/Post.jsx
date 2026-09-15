c8942137c502571f887a76feb38b895d83af67ce
Fidèle NDAYISHIMIYE <230522764+codetraildevs@users.noreply.github.com>
2026-09-15 12:26:27 +0200

    ok

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -1,10 +1,15 @@
 import PostCard from "../components/PostCard";
 
 const Post = () => {
+
+
+
+
+  
   return (
     <div style={{border:'1px solid #333',borderRadius:'5px',minHeight:'500px' ,padding:'20px',margin:'10px'}}>
         <h2 style={{fontSize:'30px',textAlign:'center'}}>Available post</h2>
-    <div style={{display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}>
+    <div style={{display: 'flex', justifyContent: 'center', gap:'20px',flexWrap:'nowrap'}}>
 
       <PostCard
         title="AI for Future education"
@@ -19,6 +24,14 @@ const Post = () => {
         body="quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
       />
       <PostCard
+        title="AI for Future education"
+        body="quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+      />
+       <PostCard
+        title="AI for Future education"
+        body="quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+      />
+       <PostCard
         title="AI for Future education"
         body="quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
       />
